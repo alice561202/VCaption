8c22d199de7239b81fbf4bc7c3f7116014300d75
motoi_nishimura <motoi.nishimura@k.s-cubism.jp>
2020-08-11 11:56:17 +0900

    マイグレファイルを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('user_id','64')->comment('ID');
+            $table->string('user_last_name','16')->comment('名前（姓）');
+            $table->string('user_first_name','16')->comment('名前（名）');
+            $table->string('user_email','128')->comment('メールアドレス');
+            $table->string('password','64')->comment('パスワード');
+            $table->string('remember_token','64')->nullable()->default('')->comment('パスワード忘れトークン');
+            $table->smallInteger('user_status')->comment('ステータス');
+            $table->timestampTz('created_at')->useCurrent()->comment('作成日時');
+            $table->string('created_by','64')->comment('作成者');
+            $table->timestampTz('updated_at')->useCurrent()->comment('更新日時');
+            $table->string('updated_by','64')->comment('更新者');
+            $table->softDeletesTz();
+            $table->primary(['user_id'], 'primary_customer');
         });
     }
 
