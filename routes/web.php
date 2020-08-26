2c14c95333e58eb92c5165349402c7de26e6e855
西村基 <motoi.nishimura@k.s-cubism.jp>
2020-08-26 10:44:39 +0900

    webルート・DB設定変更／JSの配置を変更

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//room
+Route::middleware('auth')->post('/room/new', 'RoomController@new');
+Route::middleware('auth')->post('/room/list', 'RoomController@list');
+Route::middleware('auth')->post('/room/add', 'RoomController@add');
+Route::middleware('auth')->post('/room/detail', 'RoomController@detail');
+Route::middleware('auth')->post('/room/favorite', 'RoomController@favarite');
+Route::middleware('auth')->post('/room/sort', 'RoomController@sort');
+Route::middleware('auth')->post('/room/logout', 'RoomController@logout');
+Route::middleware('auth')->post('/room/delete', 'RoomController@delete');
+
+//customer
+Route::middleware('auth')->post('/member/logout', 'MemberController@list');
+Route::middleware('auth')->post('/member/delete', 'MemberController@add');
+Route::middleware('auth')->post('/member/Reconfigure', 'MemberController@Reconfigure');
+
+//chat
+Route::middleware('auth')->post('/screen/chat', 'ScreenController@chat');
+Route::middleware('auth')->post('/screen/voice', 'ScreenController@voice');
+Route::middleware('auth')->post('/screen/video', 'ScreenController@video');
