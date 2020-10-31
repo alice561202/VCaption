1dc0657b1f216d40008e3306915dabca72a8e120
karasu-deaf <karasu.5296@gmail.com>
2020-10-31 21:32:42 +0900

    まだチャット画面での動作はできていないので、チャットテーブル作成用のPHPを挙げます。。。

---
+++ database/migrations/2020_10_18_183233_create_chat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_id')->comment('ルームID');
+            $table->string('name')->comment('名前');
+            $table->string('message')->comment('メッセージ');
+            $table->date('chat_date')->comment('登録日');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat');
+    }
+}
